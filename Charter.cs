fef45cfa215bcff90627a9a0bfa68eab4d4d3885
Benjin Dubishar <benjin.dubishar@gmail.com>
2014-07-09 17:47:43 -0400

    Cleaned up the chart

--- Charter.cs
+++ Charter.cs
@@ -25,8 +25,8 @@ namespace Loan_Projection
             area.AxisX.LineColor = Color.LightGray;
             area.AxisX.MajorGrid.LineColor = Color.LightGray;
 
-            area.AxisY.Interval = 2000000000; // 2 billion
-            area.AxisY.Title = "Amount (interval: $2B)";
+            area.AxisY.Interval = 2; // 2 billion
+            area.AxisY.Title = "Amount - $B";
             area.AxisY.LineColor = Color.LightGray;
             area.AxisY.MajorGrid.LineColor = Color.LightGray;
 
@@ -35,19 +35,29 @@ namespace Loan_Projection
             Series cash = new Series();
             cash.Color = Color.Green;
             cash.ChartType = SeriesChartType.FastLine;
-            cash.Points.DataBindXY(months, cashValues);
+            cash.Points.DataBindXY(months, scaleDown(cashValues, 9)); // 9 = 10^9 value scale downard
 
             Series debt = new Series();
             debt.Color = Color.Red;
             debt.ChartType = SeriesChartType.FastLine;
-            debt.Points.DataBindXY(months, debtValues);
+            debt.Points.DataBindXY(months, scaleDown(debtValues, 9));
 
             ch.Series.Add(cash);
             ch.Series.Add(debt);
-            
+
             FileStream output = File.Open(filename, FileMode.Create);
             ch.SaveImage(output, ChartImageFormat.Png);
             output.Close();
+        }
+
+        private static double[] scaleDown(double[] input, int amount)
+        {
+            double[] output = new double[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+                output[i] = input[i] / Math.Pow(10.0, amount);
+
+            return output;
         }
     }
 }
