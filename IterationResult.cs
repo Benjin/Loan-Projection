46581ec1f4970eaac47ffc9111e79c268d7ebbb3
Benjin Dubishar <benjin.dubishar@gmail.com>
2014-07-09 13:26:11 -0400

    Added structural classes

---
+++ IterationResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loan_Projection
+{
+    class IterationResult
+    {
+        public LoanStatus Status;
+        public double Payment;
+
+        public IterationResult(LoanStatus status, double payment)
+        {
+            this.Status = status;
+            this.Payment = payment;
+        }
+    }
+}
---
+++ LogItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loan_Projection
+{
+    class LogItem
+    {
+        public DateTime Date;
+        public double IncomingPayment = 0, OutgoingPayment = 0;
+        public double StartingCash, EndingCash = 0;
+        public double DebtChange = 0, DebtBalance = 0;
+
+        public LogItem(double startingCash, double debtBalance)
+        {
+            this.StartingCash = startingCash;
+            this.DebtBalance = debtBalance;
+        }
+
+        public void Add(IterationResult iteration)
+        {
+            if (iteration.Status == LoanStatus.Sold) { }
+
+            else if (iteration.Status == LoanStatus.Delinquent)
+            {
+
+            }
+
+            else // LoanStatus == Active
+            {
+
+            }
+        }
+    }
+}
